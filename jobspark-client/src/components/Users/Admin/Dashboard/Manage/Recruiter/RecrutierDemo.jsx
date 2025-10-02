e5267904245e4910730b6ae0db44ba02a766c764
mashayeakh <islammasayekh@gmail.com>
2025-10-02 22:33:23 +0600

    updated demo file

--- jobspark-client/src/components/Users/Admin/Dashboard/Manage/Recruiter/RecrutierDemo.jsx
+++ jobspark-client/src/components/Users/Admin/Dashboard/Manage/Recruiter/RecrutierDemo.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const RecrutierDemo = () => {
   return (
-    <div>RecrutierDemo</div>
+    <div>RecrutierDemo
+
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit culpa ipsum eos? Iste quo debitis, ipsam numquam quidem aspernatur fuga sunt ducimus accusamus quos dicta sapiente qui laboriosam ea suscipit.
+
+    </div>
   )
 }
 
