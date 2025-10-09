fbb84f2cd66fa37347d3a622e79563235c8a4937
mashayeakh <islammasayekh@gmail.com>
2025-10-09 23:45:04 +0600

    fixed issue

--- jobspark-client/src/components/Users/Admin/Dashboard/Manage/Recruiter/RecrutierDemo.jsx
+++ jobspark-client/src/components/Users/Admin/Dashboard/Manage/Recruiter/RecrutierDemo.jsx
@@ -5,7 +5,7 @@ const RecrutierDemo = () => {
     <div>RecrutierDemo
 
     Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit culpa ipsum eos? Iste quo debitis, ipsam numquam quidem aspernatur fuga sunt ducimus accusamus quos dicta sapiente qui laboriosam ea suscipit.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam laborum fuga similique numquam repellendus nobis ipsa, natus quia deleniti, veniam eum obcaecati! Iure molestiae non, ratione dolor neque ad maiores facere beatae itaque sequi sed repellendus mollitia, ipsam veritatis magnam tempora repellat error doloremque dolorem rem est, dolore temporibus architecto. Obcaecati velit enim delectus fugiat voluptas natus iste vero error amet optio laboriosam, quisquam, ratione distinusdam recusandae, consequuntur quo nostrum illo obcaecati. Eveniet possimus, corrupti laboriosam veritatis aliquam architecto! Numquam nisi similique voluptatem minima iure itaque totam quam necessiuibusdam. Nesciunt quod facere saepe aspernatur libero at ex voluptas provident.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam laborum fuga similique numquam repellendus nobis tatem minima iure itaque totam quam necessiuibusdam. Nesciunt quod facere saepe aspernatur libero at ex voluptas provident.
 
     <h1>
       Recruiter Demo Page
