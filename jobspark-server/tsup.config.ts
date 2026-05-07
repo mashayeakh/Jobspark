7ca86325c26eba1007f2cb1a7eee13aae1c90e28
mashayeakh <islammasayekh@gmail.com>
2026-05-08 00:09:08 +0600

    fix: noExternal iconv-lite in tsup config

--- jobspark-server/tsup.config.ts
+++ jobspark-server/tsup.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     platform: 'node',
     target: 'node20',
     outDir: 'dist',
-    external: ['pg-native', 'puppeteer'],
-    noExternal: ['iconv-lite', 'body-parser', 'raw-body', 'express'],
+    external: ['pg-native'],
+    noExternal: ['iconv-lite'],
 })
